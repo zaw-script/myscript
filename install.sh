517388eff832ce0f60237f72b01e028a6662c3d6
zaw-script <naveruse382@gmail.com>
2026-03-10 20:00:43 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-# ZIVPN Panel Installation
 apt update -y && apt install python3-pip -y
 pip3 install flask
-# GitHub ကနေ web.py ကို လှမ်းဆွဲယူပြီး /etc/zivpn/ ထဲကို ထည့်ပါ
+mkdir -p /etc/zivpn
 wget -O /etc/zivpn/web.py https://raw.githubusercontent.com/zaw-script/myscript/main/web.py
-# panel ကို စတင်ခြင်း
 python3 /etc/zivpn/web.py &
 echo "Panel installed successfully"
