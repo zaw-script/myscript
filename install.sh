dceca3940c18a003d8ade0c9756f44d26126aebf
zaw-script <naveruse382@gmail.com>
2026-03-11 10:44:52 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,9 @@ apt install python3-pip -y
 pip3 install flask
 mkdir -p /etc/zivpn
 wget -O /etc/zivpn/web.py https://raw.githubusercontent.com/zaw-script/myscript/main/web.py
-python3 /etc/zivpn/web.py &
-echo "Panel installed successfully"
+# အောက်က စာကြောင်းမှာ & လေး ပါတာ သေချာပါစေ (ဒါမှ အနောက်မှာ Run မှာပါ)
+python3 /etc/zivpn/web.py > /dev/null 2>&1 &
+echo "------------------------------------------"
+echo "ZIVPN Panel Installed Successfully!"
+echo "Access it at: http://$(hostname -I | awk '{print $1}'):8080"
+echo "------------------------------------------"
