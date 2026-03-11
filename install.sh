e8893d9af0b9bb75e0ae3b0d9490b371dd93112e
zaw-script <naveruse382@gmail.com>
2026-03-11 10:35:18 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-apt update -y && apt install python3-pip -y
+apt update -y
+apt install python3-pip -y
 pip3 install flask
 mkdir -p /etc/zivpn
 wget -O /etc/zivpn/web.py https://raw.githubusercontent.com/zaw-script/myscript/main/web.py
